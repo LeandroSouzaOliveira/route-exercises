fc179f28ad76b6ede1ff1bde02fee0f50f92d8e0
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-08-25 19:01:43 -0300

    Layout

---
+++ fix-exeone-routes/src/component/Button/index.tsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+type Props = {
+  text: string;
+};
+
+export default function Button({ text }: Props) {
+  return (
+    <div className="button">
+      {text}
+    </div>
+  );
+}
--- fix-exeone-routes/src/routes/Home/HomeBody/index.tsx
+++ fix-exeone-routes/src/routes/Home/HomeBody/index.tsx
@@ -1,7 +1,19 @@
+import Button from "../../../component/Button";
+import Card from "../../../component/Card";
+
 export default function HomeBody() {
-    return (
-        <main>
-            <h2>Página inicial</h2>
-        </main>
-    );
+  return (
+    <main>
+      <section>
+        <div className="container">
+          <div className="mt30 mb30">
+            <Card title="Página inicial" />
+          </div>
+          <div className="dflex">
+            <Button text="Ver promoção" />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
